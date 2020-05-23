ace54cc9ed0a24c44be4a7581aab85cdfcd50f9b
Lloyd Brookes <lloyd.brookes@gmail.com>
2020-05-23 12:18:01 +0100

    cli: validate input args

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 const getTargetUrl = require('../lib/rewrite').getTargetUrl
+const [from, to, url] = process.argv.slice(2)
 
-if (!(process.argv[2], process.argv[3], process.argv[4])) {
+if (!(from && to && url)) {
   console.log('USAGE:')
   console.log('$ lws-redirect <from> <to> <url>')
 } else {
-  console.log(getTargetUrl(process.argv[2], process.argv[3], process.argv[4]))
+  console.log(getTargetUrl(from, to, url))
 }
 
